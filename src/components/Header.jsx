af928d5532d8a65da5f28242c2e4093c58b148bb
pratik Deo <pratikdeo78@gmail.com>
2021-04-16 21:07:36 -0400

    integrated header & footer with react

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+function Header(){
+    return <header>
+    <Navbar />
+    </header>
+}
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+function Navbar(){
+    return <nav>
+    <div class="container-fluid">
+      <h1>Pratik Deo Portfolio</h1>
+    </div>
+  </nav>
+}
+
+export default Navbar;
